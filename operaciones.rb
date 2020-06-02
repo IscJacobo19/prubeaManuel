362d17d289edff1b174cc7eaeb1236413c8429a9
IscJacobo19 <iscjacobomanuel@gmail.com>
2020-06-01 19:07:15 -0500

    section02

---
+++ operaciones.rb
@@ -0,0 +1,19 @@
+#Variables
+
+print("Dame el primero numero: ")
+numero_uno = gets.chomp.to_i
+print("Dame el segundo numero: ")
+numero_dos = gets.chomp.to_i
+
+
+resultado = (numero_uno + numero_dos)
+puts "La suma de los nuemero es: #{resultado}"
+
+resultado = (numero_uno - numero_dos)
+puts "La resta de los nuemero es: #{resultado}"
+
+resultado = (numero_uno * numero_dos)
+puts "La multiplicacion de los nuemero es: #{resultado}"
+
+resultado = (numero_uno / numero_dos)
+puts "La division de los nuemero es: #{resultado}"
